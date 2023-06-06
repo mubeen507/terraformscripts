0da5e3cb1cadd4d0fcd3a5df7ec46ebca63794e9
ravi2krishna <ravi2krishna@gmail.com>
2023-06-06 02:47:11 +0000

    public subnets

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,27 @@ resource "aws_vpc" "big-vpc" {
     Name = "big-vpc"
   }
 }
+
+# Public Subnet - A
+resource "aws_subnet" "big-pub-sn-A" {
+  vpc_id     = aws_vpc.big-vpc.id
+  cidr_block = "10.0.0.0/20"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "big-pub-sn-A"
+  }
+}
+
+# Public Subnet - B
+resource "aws_subnet" "big-pub-sn-B" {
+  vpc_id     = aws_vpc.big-vpc.id
+  cidr_block = "10.0.16.0/20"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "big-pub-sn-B"
+  }
+}
