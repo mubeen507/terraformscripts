d28b36f3d5c8f9a53ff8e465fc6a303ee569fc6b
ravi2krishna <ravi2krishna@gmail.com>
2023-06-08 08:26:06 +0530

    subnet

--- vnet.tf
+++ vnet.tf
@@ -10,4 +10,11 @@ resource "azurerm_virtual_network" "tf-7am-vnet" {
   }
 }
 
+# Subnet
+resource "azurerm_subnet" "tf-7am-sn" {
+  name                 = "web-7am-subnet"
+  resource_group_name  = azurerm_resource_group.tf-7am-rg.name
+  virtual_network_name = azurerm_virtual_network.tf-7am-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
